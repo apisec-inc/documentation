4b6eaaa98b477a0723414d1ec95a5c90d3cc1150
FidaUrRahman <rahman@apisec.ai>
2023-08-29 14:32:24 +0530

    base url fix

--- docs/Chatbot/scripts.js
+++ docs/Chatbot/scripts.js
@@ -57,7 +57,7 @@ function sendMessage() {
         chatMessages.scrollTop = chatMessages.scrollHeight;
 
         // Make API call
-        const apiUrl = "https://apps.apisec.ai:8000/chat";
+        const apiUrl = "http://apps.apisec.ai:8000/chat";
         const requestBody = {
             question: userInput
         };
